f708c82a29b0273e509512c5e709136c3e457039
Mason Puckett <pucketmg@dukes.jmu.edu>
2024-12-09 15:05:21 -0500

    Script runs test cases

---
+++ code_solution/run_test_cases.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Script to run test_cases for both exact and approximate
+# Written  By: Mason Puckett and Annabelle Monahan
+
+echo "==== Test Case 4 ===="
+echo "Exact Soltion: bron-kerbosch"
+python bron-kerbosch.py test_case4
+echo
+echo
+echo "Approximation: Feige"
+python approximation.py test_case4
+echo
+echo
+
+echo "==== Test Case 6 ===="
+echo "Exact Soltion: bron-kerbosch"
+python bron-kerbosch.py test_case6
+echo
+echo
+echo "Approximation: Feige"
+python approximation.py test_case6
+echo
+echo
+
+echo "==== Test Case 10 ===="
+echo "Exact Soltion: bron-kerbosch"
+python bron-kerbosch.py test_case10
+echo
+echo
+echo "Approximation: Feige"
+python approximation.py test_case10
+echo
+echo
+
+echo "==== Test Case 16 ===="
+echo "Exact Soltion: bron-kerbosch"
+python bron-kerbosch.py test_case16
+echo
+echo
+echo "Approximation: Feige"
+python approximation.py test_case16
+echo
+echo
+
+echo "==== Test Case 20 ===="
+echo "Exact Soltion: bron-kerbosch"
+python bron-kerbosch.py test_case20
+echo
+echo
+echo "Approximation: Feige"
+python approximation.py test_case20
+echo
+echo
+
+echo "==== Test Case 22 ===="
+echo "Exact Soltion: bron-kerbosch"
+python bron-kerbosch.py test_case22
+echo
+echo
+echo "Approximation: Feige"
+python approximation.py test_case22
+echo
+echo
+
+echo "==== Test Case 25 ===="
+echo "Exact Soltion: bron-kerbosch"
+python bron-kerbosch.py test_case25
+echo
+echo
+echo "Approximation: Feige"
+python approximation.py test_case25
+echo
+echo
+
+echo "==== Test Case 26 ===="
+echo "Exact Soltion: bron-kerbosch"
+python bron-kerbosch.py test_case26
+echo
+echo
+echo "Approximation: Feige"
+python approximation.py test_case26
+echo
+echo
