3e0a88dbca959dd7cdbe0a24947f343abe01e2a3
Mason Puckett <pucketmg@dukes.jmu.edu>
2024-12-09 22:01:27 -0500

    Last push

---
+++ code_solution/variance_script.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+
+# Script to run approximation algorithm 10 times per test case to showcase any variance
+# Written By: Mason Puckett and Annabelle Monahan
+
+echo "=== Test Case 4 ==="
+echo "Run 1"
+python approximation.py test_case4
+echo "Run 2"
+python approximation.py test_case4
+echo "Run 3"
+python approximation.py test_case4
+echo "Run 4"
+python approximation.py test_case4
+echo "Run 5"
+python approximation.py test_case4
+echo "Run 6"
+python approximation.py test_case4
+echo "Run 7"
+python approximation.py test_case4
+echo "Run 8"
+python approximation.py test_case4
+echo "Run 9"
+python approximation.py test_case4
+echo "Run 10"
+python approximation.py test_case4
+echo
+echo
+
+echo "=== Test Case 6 ==="
+echo "Run 1"
+python approximation.py test_case6
+echo "Run 2"
+python approximation.py test_case6
+echo "Run 3"
+python approximation.py test_case6
+echo "Run 4"
+python approximation.py test_case6
+echo "Run 5"
+python approximation.py test_case6
+echo "Run 6"
+python approximation.py test_case6
+echo "Run 7"
+python approximation.py test_case6
+echo "Run 8"
+python approximation.py test_case6
+echo "Run 9"
+python approximation.py test_case6
+echo "Run 10"
+python approximation.py test_case6
+echo
+echo
+
+echo "=== Test Case 10 ==="
+echo "Run 1"
+python approximation.py test_case10
+echo "Run 2"
+python approximation.py test_case10
+echo "Run 3"
+python approximation.py test_case10
+echo "Run 4"
+python approximation.py test_case10
+echo "Run 5"
+python approximation.py test_case10
+echo "Run 6"
+python approximation.py test_case10
+echo "Run 7"
+python approximation.py test_case10
+echo "Run 8"
+python approximation.py test_case10
+echo "Run 9"
+python approximation.py test_case10
+echo "Run 10"
+python approximation.py test_case10
+echo
+echo
+
+echo "=== Test Case 16 ==="
+echo "Run 1"
+python approximation.py test_case16
+echo "Run 2"
+python approximation.py test_case16
+echo "Run 3"
+python approximation.py test_case16
+echo "Run 4"
+python approximation.py test_case16
+echo "Run 5"
+python approximation.py test_case16
+echo "Run 6"
+python approximation.py test_case16
+echo "Run 7"
+python approximation.py test_case16
+echo "Run 8"
+python approximation.py test_case16
+echo "Run 9"
+python approximation.py test_case16
+echo "Run 10"
+python approximation.py test_case16
+echo
+echo
+
+echo "=== Test Case 20 ==="
+echo "Run 1"
+python approximation.py test_case20
+echo "Run 2"
+python approximation.py test_case20
+echo "Run 3"
+python approximation.py test_case20
+echo "Run 4"
+python approximation.py test_case20
+echo "Run 5"
+python approximation.py test_case20
+echo "Run 6"
+python approximation.py test_case20
+echo "Run 7"
+python approximation.py test_case20
+echo "Run 8"
+python approximation.py test_case20
+echo "Run 9"
+python approximation.py test_case20
+echo "Run 10"
+python approximation.py test_case20
+echo
+echo
+
+echo "=== Test Case 22 ==="
+echo "Run 1"
+python approximation.py test_case22
+echo "Run 2"
+python approximation.py test_case22
+echo "Run 3"
+python approximation.py test_case22
+echo "Run 4"
+python approximation.py test_case22
+echo "Run 5"
+python approximation.py test_case22
+echo "Run 6"
+python approximation.py test_case22
+echo "Run 7"
+python approximation.py test_case22
+echo "Run 8"
+python approximation.py test_case22
+echo "Run 9"
+python approximation.py test_case22
+echo "Run 10"
+python approximation.py test_case22
+echo
+echo
+
+echo "=== Test Case 25 ==="
+echo "Run 1"
+python approximation.py test_case25
+echo "Run 2"
+python approximation.py test_case25
+echo "Run 3"
+python approximation.py test_case25
+echo "Run 4"
+python approximation.py test_case25
+echo "Run 5"
+python approximation.py test_case25
+echo "Run 6"
+python approximation.py test_case25
+echo "Run 7"
+python approximation.py test_case25
+echo "Run 8"
+python approximation.py test_case25
+echo "Run 9"
+python approximation.py test_case25
+echo "Run 10"
+python approximation.py test_case25
+echo
+echo
+
+echo "=== Test Case 26 ==="
+echo "Run 1"
+python approximation.py test_case26
+echo "Run 2"
+python approximation.py test_case26
+echo "Run 3"
+python approximation.py test_case26
+echo "Run 4"
+python approximation.py test_case26
+echo "Run 5"
+python approximation.py test_case26
+echo "Run 6"
+python approximation.py test_case26
+echo "Run 7"
+python approximation.py test_case26
+echo "Run 8"
+python approximation.py test_case26
+echo "Run 9"
+python approximation.py test_case26
+echo "Run 10"
+python approximation.py test_case26
+echo
+echo
